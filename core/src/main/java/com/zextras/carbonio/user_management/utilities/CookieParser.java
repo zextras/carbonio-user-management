0fc39239f5d27887c90613589f0a4a628220e0ce
Federico Rispo <federico.rispo@zextras.com>
2023-08-18 11:20:18 +0200

    test: add a unit test for CookieParser class
    - Added the CookieParserTest
    - Fixed the CookieParser#getCookies method: now it handles input strings
      that are blank or terminating with a semicolon.
    
    refs: UM-23

--- core/src/main/java/com/zextras/carbonio/user_management/utilities/CookieParser.java
+++ core/src/main/java/com/zextras/carbonio/user_management/utilities/CookieParser.java
@@ -5,14 +5,28 @@
 package com.zextras.carbonio.user_management.utilities;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CookieParser {
 
+  /**
+   * Allows to convert a {@link String} containing a list of cookies (it can be empty) to a
+   * {@link Map} where each key/value entry represent a cookie.
+   *
+   * @param cookies a {@link String} representing a list of cookies with this format:
+   *                <code>token=value; token2=value2</code>.
+   * @return a {@link Map} where each entry represent the key/value of a cookie.
+   */
   public static Map<String, String> getCookies(String cookies) {
+    if (cookies.isEmpty()) {
+      return Collections.emptyMap();
+    }
+
     return Arrays
       .stream(cookies.split(";", -1))
+      .filter(cookie -> !cookie.isBlank())
       .map(cookie -> cookie.split("=", -1))
       .collect(Collectors.toMap(
         cookie -> cookie[0].trim(),
---
+++ core/src/test/java-ut/com/zextras/carbonio/user_management/utilities/CookieParserTest.java
@@ -0,0 +1,64 @@
+package com.zextras.carbonio.user_management.utilities;
+
+import java.util.Map;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CookieParserTest {
+
+  static Stream<Arguments> cookiesProvider() {
+    return Stream.of(
+      Arguments.of("ZM_AUTH_TOKEN=fake-zm-token", Map.of("ZM_AUTH_TOKEN", "fake-zm-token")),
+      Arguments.of(
+        "ZM_AUTH_TOKEN=fake-zm-token; ZX_AUTH_TOKEN=fake-zx-token",
+        Map.of("ZM_AUTH_TOKEN", "fake-zm-token", "ZX_AUTH_TOKEN", "fake-zx-token")
+      ),
+      Arguments.of(
+        "ZM_AUTH_TOKEN=fake-zm-token; ZX_AUTH_TOKEN=fake-zx-token; SESSION=id",
+        Map.of("ZM_AUTH_TOKEN", "fake-zm-token", "ZX_AUTH_TOKEN", "fake-zx-token", "SESSION", "id")
+      )
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("cookiesProvider")
+  void givenACookiesStringContainingMultipleCookiesTheTheGetCookiesShouldReturnAMapOfThem(
+    String cookies,
+    Map<String, String> cookiesMap
+  ) {
+    // Given & When
+    Map<String, String> expectedCookiesMap = CookieParser.getCookies(cookies);
+
+    // Then
+    Assertions.assertThat(expectedCookiesMap).isEqualTo(cookiesMap);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"", "    "})
+  void givenAnEmptyCookiesStringTheGetCookiesShouldReturnAnEmptyMap(String cookies) {
+    // Given & When
+    Map<String, String> expectedCookiesMap = CookieParser.getCookies(cookies);
+
+    // Then
+    Assertions.assertThat(expectedCookiesMap).hasSize(0);
+  }
+
+  @Test
+  void givenACookiesStringTerminatingWithASemicolonTheTheGetCookiesShouldReturnAMapOfThem() {
+    // Given
+    String cookies = "ZM_AUTH_TOKEN=fake-zm-token;  ";
+    // When
+    Map<String, String> expectedCookiesMap = CookieParser.getCookies(cookies);
+
+    // Then
+    Assertions
+      .assertThat(expectedCookiesMap)
+      .hasSize(1)
+      .containsEntry("ZM_AUTH_TOKEN", "fake-zm-token");
+  }
+}
