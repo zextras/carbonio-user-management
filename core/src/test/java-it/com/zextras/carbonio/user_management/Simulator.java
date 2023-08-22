1931496ed2b1540f63378e4c119e981b5fca818b
Federico Rispo <federico.rispo@zextras.com>
2023-08-22 14:54:59 +0200

    test: add an integration test for the GetUserById API
    - Updated the poms to allow the implementation of a Simulator and to
      clean up the reduntant dependencies
    - Created a Simulator to call the APIs to test
    - Created the SoapHttpUtils class to provide a way to generate SOAP
      requests and responses without too much effort
    - Created the GetUserById API to test that everything works correctly
      (it tests only the success case)

---
+++ core/src/test/java-it/com/zextras/carbonio/user_management/Simulator.java
@@ -0,0 +1,152 @@
+package com.zextras.carbonio.user_management;
+
+import client.SoapClient;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.zextras.carbonio.user_management.config.UserManagementModule;
+import io.swagger.models.HttpMethod;
+import java.io.IOException;
+import java.util.Objects;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.jetty.server.LocalConnector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.jboss.resteasy.plugins.guice.GuiceResteasyBootstrapServletContextListener;
+import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.BinaryBody;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+
+public final class Simulator implements AutoCloseable {
+
+  private final static String MAILBOX_SERVICE_IP = "127.78.0.5";
+  private final static int MAILBOX_SERVICE_PORT = 20_000;
+  private final static String MAILBOX_SERVICE_URI =
+    String.format("http://%s:%d", MAILBOX_SERVICE_IP, MAILBOX_SERVICE_PORT);
+
+  private final Injector injector;
+  private final SoapHttpUtils soapHttpUtils;
+
+  private ClientAndServer clientAndServer;
+  private MockServerClient mailboxServiceMock;
+  private Server jettyServer;
+  private LocalConnector httpLocalConnector;
+
+  private Simulator() {
+    this.injector = Guice.createInjector(new UserManagementModule());
+    this.soapHttpUtils = new SoapHttpUtils();
+  }
+
+  private void startMailboxService() {
+    clientAndServer = ClientAndServer.startClientAndServer(MAILBOX_SERVICE_PORT);
+    mailboxServiceMock = new MockServerClient(MAILBOX_SERVICE_IP, MAILBOX_SERVICE_PORT);
+
+    try {
+      final byte[] wsdl = IOUtils.toByteArray(Objects.requireNonNull(
+        getClass().getClassLoader().getResourceAsStream("soap/ZimbraService.wsdl")
+      ));
+
+      mailboxServiceMock
+        .when(HttpRequest
+          .request()
+          .withMethod(HttpMethod.GET.toString())
+          .withPath("/service/wsdl/ZimbraService.wsdl")
+        )
+        .respond(HttpResponse.response().withStatusCode(200).withBody(BinaryBody.binary(wsdl)));
+    } catch (IOException exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+
+  private void stopMailboxService() {
+    if (mailboxServiceMock != null && mailboxServiceMock.hasStarted()) {
+      mailboxServiceMock.stop();
+      clientAndServer.stop();
+    }
+  }
+
+  private void startJettyServer() {
+    try {
+      jettyServer = new Server();
+      httpLocalConnector = new LocalConnector(jettyServer);
+      jettyServer.addConnector(httpLocalConnector);
+      ServletContextHandler servletHandler = new ServletContextHandler(jettyServer, "/");
+
+      servletHandler.addEventListener(
+        injector.getInstance(GuiceResteasyBootstrapServletContextListener.class)
+      );
+
+      ServletHolder servletHolder = new ServletHolder(HttpServletDispatcher.class);
+
+      servletHandler.addServlet(servletHolder, "/*");
+      jettyServer.setHandler(servletHandler);
+
+      jettyServer.start();
+    } catch (Exception exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+
+  private void stopJettyServer() {
+    if (jettyServer != null) {
+      try {
+        jettyServer.stop();
+      } catch (Exception exception) {
+        throw new RuntimeException(exception);
+      }
+    }
+  }
+
+  public Simulator start() {
+    SoapClient.init(MAILBOX_SERVICE_URI);
+    startJettyServer();
+    return this;
+  }
+
+  public void stopAll() {
+    stopJettyServer();
+    stopMailboxService();
+  }
+
+  @Override
+  public void close() {
+    stopAll();
+  }
+
+  public MockServerClient getMailboxServiceMock() {
+    return mailboxServiceMock;
+  }
+
+  public LocalConnector getHttpLocalConnector() {
+    return httpLocalConnector;
+  }
+
+  public SoapHttpUtils getSoapHttpUtils() {return soapHttpUtils;}
+
+  public final static class SimulatorBuilder {
+
+    private Simulator simulator;
+
+    public static SimulatorBuilder aSimulator() {
+      return new SimulatorBuilder();
+    }
+
+    public SimulatorBuilder init() {
+      simulator = new Simulator();
+      return this;
+    }
+
+    public SimulatorBuilder withMailboxService() {
+      simulator.startMailboxService();
+      return this;
+    }
+
+    public Simulator build() {
+      return simulator;
+    }
+
+  }
+}
---
+++ core/src/test/java-it/com/zextras/carbonio/user_management/SoapHttpUtils.java
@@ -0,0 +1,68 @@
+package com.zextras.carbonio.user_management;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Utility class to offer methods for generating SOAP request/response by substituting input values
+ * and expected values to request/response XML files.
+ */
+public class SoapHttpUtils {
+
+  /**
+   * Creates the GetAccountInfoRequest XML body substituting the auth token and the account id in
+   * the related placeholders.
+   *
+   * @param authToken is a {@links String} representing the auth token of the requester
+   * @param accountId is a {@link String} representing the identifier of the account to retrieve
+   * @return a {@link String} representing the XML body request for the GetAccountInfo API.
+   */
+  public String getAccountInfoRequest(String authToken, String accountId) {
+    String getInfoRequest = getXmlFile("soap/requests/GetAccountInfoRequest.xml");
+
+    return getInfoRequest
+      .replaceAll("%AUTH_TOKEN%", authToken)
+      .replaceAll("%ACCOUNT_ID%", accountId);
+  }
+
+  /**
+   * Creates the GetAccountInfoResponse XML body substituting the input parameters in the related
+   * placeholders.
+   *
+   * @param accountId       is a {@link String} representing the identifier of the retrieved
+   *                        account
+   * @param accountEmail    is a {@link String} representing the email of the retrieved account
+   * @param accountDomain   is a {@link String} representing the domain of the retrieved account
+   * @param accountFullName is a {@link String} representing the full name of the retrieved account
+   * @return a {@link String} representing the XML payload response for the GetAccountInfo API.
+   */
+  public String getAccountInfoResponse(String accountId, String accountEmail, String accountDomain,
+    String accountFullName) {
+    String getInfoRequest = getXmlFile("soap/responses/GetAccountInfoResponse.xml");
+
+    return getInfoRequest
+      .replaceAll("%ACCOUNT_ID%", accountId)
+      .replaceAll("%ACCOUNT_EMAIL%", accountEmail)
+      .replaceAll("%ACCOUNT_DOMAIN%", accountDomain)
+      .replaceAll("%ACCOUNT_FULL_NAME%", accountFullName);
+  }
+
+  /**
+   * Allows to load the request/response xml file in memory to performs some substitution.
+   *
+   * @param path is {@link String} representing the path with the filename of the file to open
+   * @return a {@link String} containing the content of the opened file without indentation.
+   */
+  private String getXmlFile(String path) {
+
+    try (InputStream resource = getClass().getClassLoader().getResourceAsStream(path)) {
+      return IOUtils
+        .toString(resource, StandardCharsets.UTF_8)
+        .replaceAll("\n( *)<", "<"); // This replacement is necessary to remove the XML indentation;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
